eb68e817ea28fcdfeba3fb686f3d05fb9aecc73f
Irakli Gozalishvili <rfobic@gmail.com>
2010-09-24 18:46:33 +0200

    adding few more tests

--- test/all.js
+++ test/all.js
@@ -45,5 +45,53 @@ exports['test:error'] = function(test) {
   )
 }
 
-run(exports)
+exports['test:pseudo async throws'] = function(test, done) {
+  run(
+    { 'test:must throw': function() {
+      throw new Error('Sorry, but you have to see error & it is not a failure')
+    }}
+  , function(result) {
+      test(assert.equal, result.passes.length, 0, 'Must not pass any test')
+      test(assert.equal, result.fails.length, 0, 'Must not fail any test')
+      test(assert.equal, result.errors.length, 1, 'Must contain one error')
+      done()
+      return false
+    }
+  )
+}
+ 
+ exports['test:pseudo async throws'] = function(test, done) {
+  run(
+    { 'test:must throw': function() {
+      throw new Error('Sorry, but you have to see error & it is not a failure')
+    }}
+  , function(result) {
+      test(assert.equal, result.passes.length, 0, 'Must not pass any test')
+      test(assert.equal, result.fails.length, 0, 'Must not fail any test')
+      test(assert.equal, result.errors.length, 1, 'Must contain one error')
+      done()
+      return false
+    }
+  )
+}
+
+exports['test:async throws'] = function(test, done) {
+  run(
+    { 'test:must throw': function() {
+      throw new Error('Sorry, but you have to see error & it is not a failure')
+    }}
+  , function(result) {
+      setTimeout(function() {
+        test(assert.equal, result.passes.length, 0, 'Must not pass any test')
+        test(assert.equal, result.fails.length, 0, 'Must not fail any test')
+        test(assert.equal, result.errors.length, 1, 'Must contain one error')
+        done()
+      })
+      return false
+    }
+  )
+}
+
+ 
+ run(exports)
 
