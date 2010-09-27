de0b8c989e8e96d836c99f20723aa1853a386eb6
Irakli Gozalishvili <rfobic@gmail.com>
2010-09-27 21:25:46 +0200

    Addding some tests for assertor methods and simlifying existing ones.

--- test/assert.js
+++ test/assert.js
@@ -3,20 +3,71 @@
 var run = require('test').run
   , Reporter = require('./utils/assert-report').Assert
 
-exports['test:pass'] = function(assert) {
+exports['test existence of all assert methods on `assert`'] = function(assert) {
   var functionType = 'function'
+    , methods =
+        [ 'ok', 'equal', 'notEqual', 'deepEqual', 'notDeepEqual', 'raises' ]
   run(
     { Assert: Reporter
-    , 'test:existence of assert methods': function($) {
+    , mute: true
+    , 'test fixture': function($) {
+        methods.forEach(function(name) {
+          assert.equal
+          ( typeof $[name]
+          , functionType
+          , '`' + name + '` must be method of `assert`'
+          )
+        })
+      }
+    }
+  )
+}
+
+exports['test correctness of `assert.ok`'] = function(assert) {
+  var truthy = [ 1, -9, true, ',.', {}, function(){}, ['1'], Infinity ]
+    , falsy = [ null, undefined, false, '', 0, NaN ]
+    , report = null
+
+  run(
+    { Assert: Reporter
+    , mute: true
+    , 'test fixture': function($) {
+        report = $.report
+        truthy.forEach(function(value, index) {
+          $.ok(value, value + ' is truthy')
+          assert.equal
+          ( report.passes.length
+          , index + 1
+          , 'The `' + value + '` must be truthy'
+          )
+        })
+
+        falsy.forEach(function(value, index) {
+          $.ok(value, value + ' is falsy')
+          assert.equal
+          ( report.fails.length
+          , index + 1
+          , 'The `' + value + '` must be falsy'
+          )
+        })
+
         assert.equal
-        ( typeof $.ok
-        , functionType
-        , '`assert` must contain `ok` assertion function.'
+        ( report.passes.length
+        , truthy.length
+        , 'All truthy properties must pass `ok` asserts'
+        )
+
+        assert.equal
+        ( report.fails.length
+        , falsy.length
+        , 'All falsy values must fail `ok` asserts'
         )
       }
     }
   )
 }
+
+
 
 run(exports)
 
