c4fcee7560379d090f6f19958f258e7d864bda92
Irakli Gozalishvili <rfobic@gmail.com>
2010-09-29 23:51:19 +0200

    Improving assert-reporter so that it reports all results.

--- test/utils/assert-report.js
+++ test/utils/assert-report.js
@@ -13,17 +13,20 @@ var AssertBase = require('test/assert').Assert
       }, configurable: true }
     , pass: { value: function pass(message) {
         this.report.passes.push(message)
+        Object.getPrototypeOf(this).pass.call(this, message)
       }}
     , fail: { value: function fail(e) {
         this.report.fails.push(e)
+        Object.getPrototypeOf(this).fail.call(this, e)
       }}
     , error: { value: function error(e) {
         this.report.errors.push(e)
+        Object.getPrototypeOf(this).error.call(this, e)
       }}
     }
 
 function Assert() {
-  return Object.create(AssertBase(), AssertDescriptor)
+  return Object.create(AssertBase.apply(null, arguments), AssertDescriptor)
 }
 exports.Assert = Assert
 
