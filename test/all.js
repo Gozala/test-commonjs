3977fc876ebbb7312d9a7b6075ecb2ceba469a90
Irakli Gozalishvili <rfobic@gmail.com>
2010-10-07 16:07:38 +0200

    Makeing custom `Assert`'s bundled with test suite specific to them instead of expecting a global one in the root.

--- test/all.js
+++ test/all.js
@@ -3,5 +3,6 @@
 exports['test fail fast'] = require('./fail-fast')
 exports['test async'] = require('./async')
 exports['test assertions'] = require('./assert')
+exports['test custom `Assert`\'s'] = require('./custom-asserts')
 
 if (module == require.main) require('test').run(exports)
