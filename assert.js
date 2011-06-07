45063e9ad86a379226bbbfff45c94d61696e57a8
Irakli Gozalishvili <rfobic@gmail.com>
2011-06-07 14:04:14 +0200

    Update asserts and factor out utilities shared with logger.

--- assert.js
+++ assert.js
@@ -1,10 +1,14 @@
 /* vim:set ts=2 sw=2 sts=2 expandtab */
 /*jshint asi: true newcap: true undef: true es5: true node: true devel: true
-         forin: true */
+         forin: false */
 /*global define: true */
 
+(typeof define === "undefined" ? function ($) { $(require, exports, module) } : define)(function (require, exports, module, undefined) {
 
-'use strict';
+"use strict";
+
+var utils = require("./utils")
+
 
 /**
  * The `AssertionError` is defined in assert.
@@ -17,301 +21,313 @@
  *  })
  */
 function AssertionError(options) {
-  if ('string' === typeof options) options = { message: options }
-  this.message = options.message
-  if ('actual' in options) this.actual = options.actual
-  if ('expected' in options) this.expected = options.expected
-  if ('operator' in options) this.operator = options.operator
-  this.stack = new Error().stack
+  var assertionError = Object.create(AssertionError.prototype);
 
-  // V8 specific
-  if (Error.captureStackTrace) Error.captureStackTrace(this, console.log)
+  if (utils.isString(options))
+    options = { message: options };
+  if ("actual" in options)
+    assertionError.actual = options.actual;
+  if ("expected" in options)
+    assertionError.expected = options.expected;
+  if ("operator" in options)
+    assertionError.operator = options.operator;
+
+  assertionError.message = options.message;
+  assertionError.stack = new Error().stack;
+  return assertionError;
 }
-AssertionError.prototype = Object.create(Error.prototype,
-{ constructor: { value: AssertionError }
-, name: { value: 'AssertionError' }
-, message: { value: undefined }
-, stack: { value: undefined }
-, toString: { value: function toString() {
-    return this.message ? this.name + " : " + this.message :
-      [ this.name + ":"
-        , represent(this.expected)
-        , this.operator
-        , represent(this.actual)
-      ].join(" ")
+AssertionError.prototype = Object.create(Error.prototype, {
+  constructor: { value: AssertionError },
+  name: { value: "AssertionError", enumerable: true },
+  toString: { value: function toString() {
+    var value;
+    if (this.message) {
+      value = this.name + " : " + this.message;
+    }
+    else {
+      value = [
+        this.name + " : ",
+        utils.source(this.expected),
+        this.operator,
+        utils.source(this.actual)
+      ].join(" ");
+    }
+    return value;
   }}
-, toSource: { value: function toSource() {
-    return "new (require('assert').AssertionError)(" + _source(this) + ")"
-  }}
-})
-exports.AssertionError = AssertionError
+});
+exports.AssertionError = AssertionError;
 
 function Assert(logger) {
-  return Object.create(Assert.prototype, { _log: { value: logger }})
+  return Object.create(Assert.prototype, { _log: { value: logger }});
 }
-Assert.prototype =
-{ fail: function fail(e) {
-    this._log.fail(new AssertionError(e))
-  }
-, pass: function pass(message) {
-    this._log.pass(message)
-  }
-, error: function error(e) {
-    this._log.error(e)
-  }
-, ok: function ok(value, message) {
-    if (!!!value) this.fail(
-      { actual: value
-      , expected: true
-      , message: message
-      , operator: '=='
-      })
-    else this.pass(message)
-  }
+Assert.prototype = {
+  fail: function fail(e) {
+    this._log.fail(e);
+  },
+  pass: function pass(message) {
+    this._log.pass(message);
+  },
+  error: function error(e) {
+    this._log.error(e);
+  },
+  ok: function ok(value, message) {
+    if (!!!value) {
+      this.fail({
+        actual: value,
+        expected: true,
+        message: message,
+        operator: "=="
+      });
+    }
+    else {
+      this.pass(message);
+    }
+  },
+
   /**
-   * The equality assertion tests shallow, coercive equality with `=`.
+   * The equality assertion tests shallow, coercive equality with `==`.
    * @example
-   *    assert.equal(actual, expected, message_opt)
+   *    assert.equal(1, 1, "one is one");
    */
-, equal: function equal(actual, expected, message) {
-    if (actual != expected) this.fail(
-      { actual: actual,
+  equal: function equal(actual, expected, message) {
+    if (actual == expected) {
+      this.pass(message);
+    }
+    else {
+      this.fail({
+        actual: actual,
         expected: expected,
         message: message,
-        operator: '=='
-      })
-    else this.pass(message)
-  }
+        operator: "=="
+      });
+    }
+  },
+
   /**
    * The non-equality assertion tests for whether two objects are not equal
    * with `!=`.
    * @example
-   *    assert.notEqual(actual, expected, message_opt)
+   *    assert.notEqual(1, 2, "one is not two");
    */
-, notEqual: function notEqual(actual, expected, message) {
-    if (actual == expected) this.fail(
-      { actual: actual
-      , expected: expected
-      , message: message
-      , operator: '!='
-      })
-    else this.pass(message)
-  }
+  notEqual: function notEqual(actual, expected, message) {
+    if (actual != expected) {
+      this.pass(message);
+    }
+    else {
+      this.fail({
+        actual: actual,
+        expected: expected,
+        message: message,
+        operator: "!=",
+      });
+    }
+  },
+
   /**
-   * The equivalence assertion tests a deep equality relation.
+   * The equivalence assertion tests a deep (with `===`) equality relation.
    * @example
-   *    assert.deepEqual(actual, expected, message_opt)
+   *    assert.deepEqual({ a: "foo" }, { a: "foo" }, "equivalent objects")
    */
-, deepEqual: function deepEqual(actual, expected, message) {
-    if (!_deepEqual(actual, expected)) this.fail(
-      { actual: actual
-      , expected: expected
-      , message: message
-      , operator: 'deepEqual'
-      })
-    else this.pass(message)
-  }
+   deepEqual: function deepEqual(actual, expected, message) {
+    if (isDeepEqual(actual, expected)) {
+      this.pass(message);
+    }
+    else {
+      this.fail({
+        actual: actual,
+        expected: expected,
+        message: message,
+        operator: "deepEqual"
+      });
+    }
+  },
+
   /**
-   * The non-equivalence assertion tests for any deep inequality.
+   * The non-equivalence assertion tests for any deep (with `===`) inequality.
    * @example
-   *    assert.notDeepEqual(actual, expected, message_opt)
+   *    assert.notDeepEqual({ a: "foo" }, Object.create({ a: "foo" }),
+   *                        "object's inherit from different prototypes");
    */
-, notDeepEqual: function notDeepEqual(actual, expected, message) {
-    if (_deepEqual(actual, expected)) this.fail(
-      { actual: actual
-      , expected: expected
-      , message: message
-      , operator: "notDeepEqual"
-      })
-    else pass(message)
-  }
+  notDeepEqual: function notDeepEqual(actual, expected, message) {
+    if (!isDeepEqual(actual, expected)) {
+      this.pass(message);
+    }
+    else {
+      this.fail({
+        actual: actual,
+        expected: expected,
+        message: message,
+        operator: "notDeepEqual"
+      });
+    }
+  },
+
   /**
    * The strict equality assertion tests strict equality, as determined by
    * `===`.
    * @example
-   *    assert.strictEqual(actual, expected, message_opt)
+   *    assert.strictEqual(null, null, "`null` is `null`")
    */
-, strictEqual: function strictEqual(actual, expected, message) {
-    if (actual !== expected) this.fail(
-      { actual: actual
-      , expected: expected
-      , message: message
-      , operator: '==='
-      })
-    else this.pass(message)
-  }
-  // 10. The strict non-equality assertion tests for strict inequality, as
-  // determined by !==.assert.notStrictEqual(actual, expected, message_opt);
-, notStrictEqual: function notStrictEqual(actual, expected, message) {
-    if (actual === expected) this.fail(
-      { actual: actual
-      , expected: expected
-      , message: message
-      , operator: '!=='
-      }
-    )
-    else this.pass(message)
-  }
-  // 11. Expected to throw an error:
-  // assert.throws(block, Error_opt, message_opt);
-, throws: function raises(block, Error, message) {
-    var threw = false,
-        exception = null;
-    // (block)
-    // (block, message:String)
-    // (block, Error)
-    // (block, Error, message)
-    if ('string' == typeof Error && undefined == message) {
-      message = Error
-      Error = undefined
+  strictEqual: function strictEqual(actual, expected, message) {
+    if (actual === expected) {
+      this.pass(message);
     }
-    try {
-      block()
-    } catch (e) {
-      threw = true
-      exception = e
-    }
-    if ( threw &&
-        (  !Error
-        || (  'string' == typeof Error
-           && 0 <= exception.message.indexOf(Error)
-           )
-        || (  'function' == typeof Error
-           && 'RegExp' == Error.constructor.name
-           && Error.test(exception.message)
-           )
-        || (  'function' == typeof exception
-           && exception instanceof Error
-           )
-        )
-       ) this.pass(message)
     else {
-      var failure =
-          { message: message
-          , operator: "throws"
-          }
-
-      if (exception) failure.actual = exception
-      if (Error) failure.expected = Error
-      this.fail(failure)
+      this.fail({
+        actual: actual,
+        expected: expected,
+        message: message,
+        operator: "==="
+      });
     }
-  }
-}
-exports.Assert = Assert
+  },
 
   /**
-   * Internal utility function that is used to generate
-   * textual representation of things passed to it.
+   * The strict non-equality assertion tests for strict inequality, as
+   * determined by `!==`.
+   * @example
+   *    assert.notStrictEqual(null, undefined, "`null` is not `undefined`");
    */
-  function represent(thing) {
-    var result
-    switch(typeof(thing)) {
-      case "string":
-        result = '"' + thing + '"'
-        break
-      case "number":
-        result = thing
-        break
-      case "object":
-        if (null === thing) {
-          result = 'null'
-          break
-        }
-        var names = []
-        for(var name in thing) names.push(name)
-        result = thing
-        if (names.length > 0) {
-          result += " - {"
-          result += names.slice(0, 7).map(function(n) {
-            var repr = n + ": "
-            try {
-              repr += 'object' == typeof thing[n] ? "{...}" : represent(thing[n])
-            } catch(e) {
-              repr += "[Exception!]";
-            }
-            return repr
-          }).join(", ")
-          if (names.length > 7) result += ", ..."
-          result += "}"
-        }
-        break
-      case "function":
-        result = thing.toString().split('\n').slice(0, 2).join('\n') + '\n...}'
-        break
-      default:
-        result = '' + thing
-     }
-     return result
+  notStrictEqual: function notStrictEqual(actual, expected, message) {
+    if (actual !== expected) {
+      this.pass(message);
+    }
+    else {
+      this.fail({
+        actual: actual,
+        expected: expected,
+        message: message,
+        operator: "!=="
+      })
+    }
+  },
+
+  /**
+   * The assertion whether or not given `block` throws an exception. If optional
+   * `Error` argument is provided and it's type of function thrown error is
+   * asserted to be an instance of it, if type of `Error` is string then message
+   * of throw exception is asserted to contain it.
+   * @param {Function} block
+   *    Function that is expected to throw.
+   * @param {Error|RegExp} [Error]
+   *    Error constructor that is expected to be thrown or a string that
+   *    must be contained by a message of the thrown exception, or a RegExp
+   *    matching a message of the thrown exception.
+   * @param {String} message
+   *    Description message
+   *
+   * @examples
+   *
+   *    assert.throws(function block() {
+   *      doSomething(4)
+   *    }, "Object is expected", "Incorrect argument is passed");
+   *
+   *    assert.throws(function block() {
+   *      Object.create(5)
+   *    }, TypeError, "TypeError is thrown");
+   */
+  throws: function throws(block, Error, message) {
+    var threw = false;
+    var exception = null;
+
+    // If third argument is not provided and second argument is a string it
+    // means that optional `Error` argument was not passed, so we shift
+    // arguments.
+    if (utils.isString(Error) && utils.isUndefined(message)) {
+      message = Error;
+      Error = undefined;
+    }
+
+    // Executing given `block`.
+    try {
+      block();
+    }
+    catch (e) {
+      threw = true;
+      exception = e;
+    }
+
+    // If exception was thrown and `Error` argument was not passed assert is
+    // passed.
+    if (threw && (utils.isUndefined(Error) ||
+                 // If passed `Error` is RegExp using it's test method to
+                 // assert thrown exception message.
+                 (utils.isRegExp(Error) && Error.test(exception.message)) ||
+                 // If passed `Error` is a constructor function testing if
+                 // thrown exception is an instance of it.
+                 (utils.isFunction(Error) && utils.instanceOf(exception, Error))))
+    {
+      this.pass(message);
+    }
+
+    // Otherwise we report assertion failure.
+    else {
+      var failure = {
+        message: message,
+        operator: "throws"
+      };
+
+      if (exception)
+        failure.actual = exception;
+
+      if (Error)
+        failure.expected = Error;
+
+      this.fail(failure);
+    }
+  }
+};
+exports.Assert = Assert;
+
+function isDeepEqual(actual, expected) {
+
+  // 7.1. All identical values are equivalent, as determined by ===.
+  if (actual === expected) {
+    return true;
   }
 
-
-var _toSource = Object.prototype.toSource;
-function _source(thing) {
-  if (_toSource) return _toSource.call(thing)
-  throw new Error('Is not implemented yet')
-}
-function _deepEqual(actual, expected) {
-  // 7.1. All identical values are equivalent, as determined by ===.
-  if (actual === expected) return true
   // 7.2. If the expected value is a Date object, the actual value is
   // equivalent if it is also a Date object that refers to the same time.
-  else if (
-    ( actual instanceof Date && expected instanceof Date ) ||
-    (
-      typeof actual == typeof expected && typeof actual == 'object' &&
-      actual.constructor && expected.constructor &&
-      'Date' == actual.constructor.name && 'Date' == expected.constructor.name
-    )
-  ) return actual.getTime() === expected.getTime()
+  else if (utils.isDate(actual) && utils.isDate(expected)) {
+    return actual.getTime() === expected.getTime();
+  }
+
+  // XXX specification bug: this should be specified
+  else if (utils.isPrimitive(actual) || utils.isPrimitive(expected)) {
+    return expected === actual;
+  }
+
   // 7.3. Other pairs that do not both pass typeof value == "object",
   // equivalence is determined by ==.
-  else if ('object' != typeof actual && 'object' != typeof expected)
+  else if (!utils.isObject(actual) && !utils.isObject(expected)) {
     return actual == expected;
-  // XXX specification bug: this should be specified
-  else if ('string' == typeof expected || 'string' == typeof actual)
-    return expected === actual;
+  }
+
   // 7.4. For all other Object pairs, including Array objects, equivalence is
   // determined by having the same number of owned properties (as verified
   // with Object.prototype.hasOwnProperty.call), the same set of keys
   // (although not necessarily the same order), equivalent values for every
   // corresponding key, and an identical "prototype" property. Note: this
   // accounts for both named and indexed properties on Arrays.
-  else return (
-    actual.prototype === expected.prototype && _equiv(actual, expected)
-  )
-}
-function _equiv(a, b, stack) {
-  console.log(">>>>>>>>>")
-  return (
-    a !== null && a !== undefined &&
-    b !== null && b !== undefined &&
-    _equivArray(Object.keys(a).sort(), Object.keys(b).sort()) &&
-    Object.keys(a).every(function(key) {
-      return _deepEqual(a[key], b[key], stack)
-    })
-  )
+  else {
+    return actual.prototype === expected.prototype &&
+           isEquivalent(actual, expected);
+  }
 }
 
-function _arrayLike(thing) {
-  return (
-    (Array.isArray && Array.isArray(thing)) ||
-    ('[object Array]' == Object.prototype.toString.call(thing)) ||
-    ('[object Arguments]' == Object.prototype.toString.call(thing)) ||
-    (
-      'object' == typeof thing &&
-      Object.prototype.hasOwnProperty.call(object, 'callee') &&
-      '[object Function]' == Object.prototype.toString.call(thing.callee) &&
-      'number' == typeof thing.length
-    )
-  )
-}
-function _equivArray(a, b, stack) {
-  return (
-    _arrayLike(a) &&
-    _arrayLike(b) &&
-    Array.prototype.every.call(a, function(value, index) {
-      return _deepEqual(value, b[index])
-    })
-  )
+function isEquivalent(a, b, stack) {
+  return isArrayEquivalent(Object.keys(a).sort(),
+                           Object.keys(b).sort()) &&
+          Object.keys(a).every(function(key) {
+            return isDeepEqual(a[key], b[key], stack)
+          });
 }
 
+function isArrayEquivalent(a, b, stack) {
+  return utils.isArray(a) && utils.isArray(b) &&
+         a.every(function(value, index) {
+           return isDeepEqual(value, b[index]);
+         });
+}
+
+});
--- log.js
+++ log.js
@@ -2,6 +2,13 @@
 /*jshint asi: true newcap: true undef: true es5: true node: true devel: true
          forin: false */
 /*global define: true */
+
+
+(typeof define === "undefined" ? function ($) { $(require, exports, module) } : define)(function (require, exports, module, undefined) {
+
+
+var utils = require('./utils.js')
+
 var COLOR_BLANK = '\033[0;30m',
     COLOR_RED = '\033[0;31m',
     COLOR_GREEN = '\033[0;32m',
@@ -34,67 +41,6 @@ function Indent(message, indent) {
   return message.replace(/^/gm, indent)
 }
 
-/**
- * Internal utility function that is used to generate
- * textual representation of things passed to it.
- */
-
-function toSource(thing, indent, visited) {
-  indent = undefined !== indent ? indent + '  ' : ''
-  var result, index, root
-  if (!visited) {
-    root = true
-    visited = []
-  }
-  index = visited.indexOf(thing)
-  if (0 <= index) return '#' + ++index + '#'
-  switch (typeof (thing)) {
-  case 'string':
-    result = '"' + thing + '"'
-    break
-  case 'number':
-    result = '' + thing
-    break
-  case 'object':
-    if (null === thing) {
-      result = 'null'
-      break
-    }
-    if (Array.isArray(thing)) {
-      result = '['
-      result += thing.map(function ($) {
-        return toSource($, indent, visited)
-      }).join(',')
-      result += ']'
-      break
-    }
-    visited.push(thing)
-    var names = []
-    for (var name in thing) names.push(name)
-    result = thing + '' !== {} + '' ? '/* ' + thing + ' */' : ''
-    if (names.length > 0) {
-      result += '\n' + indent + '{ '
-      result += names.map(function (name) {
-        var repr = '\'' + name + '\': '
-        try {
-          repr += toSource(thing[name], indent, visited)
-        } catch (e) {
-          repr += '[Exception!]'
-        }
-        return repr
-      }).join('\n' + indent + ', ')
-      result += '\n' + indent + '}'
-    }
-    break
-  case "function":
-    result = thing.toString().split('\n').join('\n' + indent)
-    break
-  default:
-    result = '' + thing
-  }
-  return root && '\n' === result.charAt(0) ? result.substr(1) : result
-}
-
 function Log(options) {
   options = options || {}
   return Object.create(Log.prototype, {
@@ -117,13 +63,16 @@ Log.prototype = {
   },
   fail: function fail(e) {
     var message = e.message
-    if ('expected' in e) message += '\n  Expected: \n' + toSource(e.expected, INDENT)
-    if ('actual' in e) message += '\n  Actual: \n' + toSource(e.actual, INDENT)
-    if ('operator' in e) message += '\n  Operator: ' + toSource(e.operator, INDENT)
+    if ('expected' in e)
+      message += '\n  Expected: \n' + Indent(utils.source(e.expected))
+    if ('actual' in e)
+      message += '\n  Actual: \n' + Indent(utils.source(e.actual))
+    if ('operator' in e)
+      message += '\n  Operator: ' + Indent(utils.source(e.operator))
     this.print(Indent(Color(COLOR_FAIL, ICON_FAIL + message)))
   },
   error: function error(e) {
-    this.print(Indent(Color(COLOR_ERROR, ICON_ERROR + toSource(e))))
+    this.print(Indent(Color(COLOR_ERROR, ICON_ERROR + (e.stack || e))))
   },
   section: function section(options) {
     options = options || {}
@@ -133,3 +82,4 @@ Log.prototype = {
 }
 exports.Log = Log
 
+});
