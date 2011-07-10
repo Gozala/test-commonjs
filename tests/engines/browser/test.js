320e6030444a9110ae74588306aeae702e58abd1
Irakli Gozalishvili <rfobic@gmail.com>
2011-07-10 16:49:22 +0200

    Splitting up tests into engines as well.

--- tests/engines/browser/test.js
+++ tests/engines/browser/test.js
@@ -7,7 +7,12 @@
 
 'use strict';
 
+exports['test async'] = require('../../async')
+exports['test assertions'] = require('../../assert')
+exports['test custom `Assert`\'s'] = require('../../custom-asserts')
+
+
 if (module == require.main)
-  require('../engines/node/test').run(require('./all'))
+  require('../../../engines/browser/test').run(exports)
 
 });
--- tests/engines/node/test.js
+++ tests/engines/node/test.js
@@ -7,9 +7,12 @@
 
 'use strict';
 
-exports['test fail fast'] = require('./fail-fast')
-exports['test async'] = require('./async')
-exports['test assertions'] = require('./assert')
-exports['test custom `Assert`\'s'] = require('./custom-asserts')
+exports['test fail fast'] = require('../../fail-fast')
+exports['test async'] = require('../../async')
+exports['test assertions'] = require('../../assert')
+exports['test custom `Assert`\'s'] = require('../../custom-asserts')
+
+if (module == require.main)
+  require('../../../engines/node/test').run(exports)
 
 });
